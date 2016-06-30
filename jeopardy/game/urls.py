10e07f7cd94370e049e8c5d6cf2d2a646bcb856a
asimonia <alex.simonian@gmail.com>
2016-06-29 22:31:51 -0500

    delete

--- jeopardy/game/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-url(r'^$', views.index, name='index'),
-url(r'^(?P<air_date>\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01]))/$', views.date, name='date'),
-]
